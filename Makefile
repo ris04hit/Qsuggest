ec1e9012ee376483750ad4ec9700a323988102d9
ris04hit <ris04hit@gmail.com>
2023-11-30 15:12:40 +0000

    Scraped problems.csv

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 scrape:
 	make scrape_user
+	make scrape_problem
 
 scrape_user:
 	python3 src/data/scrape_data.py user
+
+scrape_problem:
+	python3 src/data/scrape_data.py problem
--- src/data/scrape_data.py
+++ src/data/scrape_data.py
@@ -20,9 +20,14 @@ def api_url(methodName, contestID = None, gym = False, handle = None, handles = 
         return f'{url}/contest.list?gym={gym}'
     
     # contest standing
-    # args:: contestId, showUnofficial
+    # args: contestId, showUnofficial
     if methodName == 'cs':
         return f'{url}/contest.standings?contestId={contestID}&showUnofficial={showUnofficial}'
+    
+    # problemset problems
+    # args: None
+    if methodName == 'pp':
+        return f'{url}/problemset.problems'
     
     # user rating          
     # args: handle
@@ -53,10 +58,11 @@ async def user_data():
     async with aiohttp.ClientSession() as session:
         with open('logs/scrape_user.txt', 'w') as sys.stdout:
             start_time = time.time()
-            batch_size = 6
-            request_limit = 140
-            sleep_time = 140
+            batch_size = 6              # Number of parallel requests
+            request_limit = 140         # Number of request after which program sleeps temporarily
+            sleep_time = 140            # Sleep time for preventing server overwhelming
             
+            # Requesting Contests
             contest_list_request = asyncio.create_task(request_json(session, api_url('cl')))
             contest_list = await asyncio.gather(contest_list_request)
             printf(f"Contest List Fetched\t\tTime Taken: {time.time() - start_time}")
@@ -68,10 +74,11 @@ async def user_data():
                 if contest['startTimeSeconds'] + contest['durationSeconds'] < time.time():       # Ensuring the contest has ended
                     standings_request.append(asyncio.create_task(request_json(session, api_url('cs', contestID = contest['id']))))
                 if (batch_size == len(standings_request)) or (loop_ind == len(contest_list[0])):
+                    # Fetching data for standings of contest
                     standings = await asyncio.gather(*standings_request)
                     standings_request = []
                     for stand in standings:
-                        if stand is not None:
+                        if stand is not None:       # If there was no error in fetching Data
                             for rank_list_row in stand['rows']:
                                 for member in rank_list_row['party']['members']:
                                     users.add(member['handle'])
@@ -81,13 +88,36 @@ async def user_data():
                     time.sleep(sleep_time)
             df = pd.DataFrame({'handle': sorted(list(users))})
             df.to_csv('data/raw/scrapped_data/handles.csv', index = False)
-        print('handles.csv created')
+            
+        sys.stdout = sys.__stdout__
+        printf('handles.csv created')
 
+
+# Creating problems.csv and tag.csv
+async def problem_data():
+    async with aiohttp.ClientSession() as session:
+        with open('logs/scrape_problem.txt', 'w') as sys.stdout:
+            start_time = time.time()
+            
+            problem_list_request = asyncio.create_task(request_json(session, api_url('pp')))
+            problem_list = await asyncio.gather(problem_list_request)
+            printf(f"Problem List Fetched\t\tTime Taken: {time.time() - start_time}")
+            
+            df_problem = pd.DataFrame(problem_list[0]['problems'])
+            df_statistics = pd.DataFrame(problem_list[0]['problemStatistics'])
+            df = pd.concat((df_problem.drop(columns=['points']), df_statistics.drop(columns=['contestId', 'index'])), axis=1)
+            df.to_csv('data/raw/scrapped_data/problems.csv', index = False)
+
+        sys.stdout = sys.__stdout__
+        printf('problems.csv created')
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         printf("Requires more command line arguments")
         sys.exit()
     
-    if sys.argv[1] == 'user':       # Generate user.csv
+    if sys.argv[1] == 'user':       # Generate handles.csv
         asyncio.run(user_data())
+    
+    if sys.argv[1] == 'problem':    # Generate problems.csv
+        asyncio.run(problem_data())
